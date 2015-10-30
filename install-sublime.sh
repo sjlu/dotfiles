02c23d2ff427b8cc94cf40f4aec4324f7a996292
Steven Lu <tacticalazn@gmail.com>
2015-10-30 16:55:26 -0400

    more install script

---
+++ install-sublime.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+rm -r User
+ln -s ~/Dropbox/Sublime/User
--- install.sh
+++ install.sh
@@ -10,6 +10,11 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew tap homebrew/services
 brew install mysql redis mongodb git wget vim bash bash-completion
-brew install caskroom/cask/brew-cask
-brew cask install google-chrome sublime-text dropbox google-drive viscosity flux spotify istat-menus 1password mattr-slate
 
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+brew cask install google-chrome sublime-text3 dropbox google-drive flux spotify istat-menus 1password mattr-slate
+
+# install slate to launch item
+osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Slate.app", hidden:true, name:"Slate"}'
+open /Applications/Slate.app
